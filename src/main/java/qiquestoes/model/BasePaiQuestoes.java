58ee508688de13932dd21fb13b9d1d4116255f1a
SmythyCosta <smythy.costa@hotmail.com>
2019-07-03 01:31:20 -0300

    BasePaiQuestoes

---
+++ src/main/java/qiquestoes/model/BasePaiQuestoes.java
@@ -0,0 +1,24 @@
+package qiquestoes.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class BasePaiQuestoes<ID extends Serializable> implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private ID id;
+	
+	@Column(name = "nome", nullable = false)
+	private String nome;
+
+}
