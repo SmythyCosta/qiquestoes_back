314821ba87a68651296d4b484b431430c67365f6
SmythyCosta <smythy.costa@hotmail.com>
2019-07-06 17:00:36 -0300

    comentando o bug em Base DAO

--- src/main/java/qiquestoes/persistence/dao/BaseDao.java
+++ src/main/java/qiquestoes/persistence/dao/BaseDao.java
@@ -5,6 +5,11 @@ import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+/*
+ * Bug: spring-data: Not an managed type: class java.lang.Object 
+ * https://stackoverflow.com/questions/23812458/spring-data-not-an-managed-type-class-java-lang-object
+ * 
+ * */
 @NoRepositoryBean
 public interface BaseDao<T> extends JpaRepository<T, Serializable> {
 
