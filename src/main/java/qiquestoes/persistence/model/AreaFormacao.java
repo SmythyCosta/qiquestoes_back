66a528f3975847c497d7f79cbd036f21178b802f
SmythyCosta <smythy.costa@hotmail.com>
2019-07-06 16:39:02 -0300

    refactory AreaFormacao

--- src/main/java/qiquestoes/persistence/model/AreaFormacao.java
+++ src/main/java/qiquestoes/persistence/model/AreaFormacao.java
@@ -9,37 +9,23 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @Table(name = "tbl_area_formacao", uniqueConstraints = { @UniqueConstraint(columnNames = { "nome" }) })
 public class AreaFormacao extends BaseAFormacaoDisciplinaAssunto<Long> {
-	
+
 	private static final long serialVersionUID = 4363125340287518187L;
-	
+
 	@OneToMany(mappedBy = "areaFormacao", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JsonManagedReference
 	private List<Disciplina> disciplina;
-	
-	public AreaFormacao() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public List<Disciplina> getDisciplina() {
-		return disciplina;
-	}
-
-	public void setDisciplina(List<Disciplina> disciplina) {
-		this.disciplina = disciplina;
-	}
-
-	@Override
-	public String toString() {
-		return "AreaFormacao [disciplina=" + disciplina + "]";
-	}
-	
-	
 }
