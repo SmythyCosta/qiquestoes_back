c1c4206d773a7677a87bd8fc7680686f21c23bd4
SmythyCosta <smythy.costa@hotmail.com>
2019-07-06 23:30:47 -0300

    PerguntaOpcaoController

---
+++ src/main/java/qiquestoes/controller/PerguntaOpcaoController.java
@@ -0,0 +1,16 @@
+package qiquestoes.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import qiquestoes.persistence.model.PerguntaOpcao;
+
+@RestController
+@RequestMapping(value = "/api/pergunta")
+@CrossOrigin(origins = "*")
+@Api()
+public class PerguntaOpcaoController extends GenericRestController<PerguntaOpcao> {
+
+}
