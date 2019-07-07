2afeff5867910972cbceaf27338d74af7819f6a9
SmythyCosta <smythy.costa@hotmail.com>
2019-07-06 22:12:52 -0300

    trazendo a list de Asuunto

--- src/main/java/qiquestoes/persistence/model/Disciplina.java
+++ src/main/java/qiquestoes/persistence/model/Disciplina.java
@@ -1,12 +1,17 @@
 package qiquestoes.persistence.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,5 +29,9 @@ public class Disciplina extends BaseAFormacaoDisciplinaAssunto<Long> {
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JsonBackReference
 	private AreaFormacao areaFormacao;
+	
+	@OneToMany(mappedBy = "disciplina", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<Assunto> assunto;
 	
 }
