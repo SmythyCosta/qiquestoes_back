995e00d4f21dbe775d96942c7e4a1a2285858d44
SmythyCosta <smythy.costa@hotmail.com>
2019-07-08 06:37:59 -0300

    listando as opcoes de respostas por perguntaId

--- src/main/java/qiquestoes/controller/PerguntaOpcaoRespostaController.java
+++ src/main/java/qiquestoes/controller/PerguntaOpcaoRespostaController.java
@@ -1,10 +1,17 @@
 package qiquestoes.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DaoSupport;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import qiquestoes.persistence.dao.PerguntaOpcaoDao;
 import qiquestoes.persistence.model.PerguntaOpcaoResposta;
 
 @RestController
@@ -12,5 +19,20 @@ import qiquestoes.persistence.model.PerguntaOpcaoResposta;
 @CrossOrigin(origins = "*")
 @Api()
 public class PerguntaOpcaoRespostaController extends GenericRestController<PerguntaOpcaoResposta> {
-
+	
+	@Autowired
+	PerguntaOpcaoDao d;
+	
+	/**
+	 * Retorna a listagem de opcoes de respostas por perguntaId.
+	 * 
+	 * @param perguntaId
+	 * @return List<PerguntaOpcaoResposta>
+	 */
+	@GetMapping(value = "/pergunta/{perguntaId}")
+	public List<PerguntaOpcaoResposta> listarPorPerguntaId(@PathVariable("perguntaId") Long perguntaId) {
+				
+		return d.findByPerguntaId(perguntaId);
+	}
+	
 }
--- src/main/java/qiquestoes/persistence/dao/PerguntaOpcaoDao.java
+++ src/main/java/qiquestoes/persistence/dao/PerguntaOpcaoDao.java
@@ -1,8 +1,20 @@
 package qiquestoes.persistence.dao;
 
+import java.util.List;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.springframework.data.repository.query.Param;
+
 import qiquestoes.persistence.model.PerguntaOpcaoResposta;
 
 
+@NamedQueries({
+	@NamedQuery(name = "PerguntaOpcaoDao.findByPerguntaId", 
+			query = "SELECT op FROM PerguntaOpcaoResposta op WHERE op.pergunta.id = :perguntaId") })
 public interface PerguntaOpcaoDao extends BaseDao<PerguntaOpcaoResposta> {
+
+	List<PerguntaOpcaoResposta> findByPerguntaId(@Param("perguntaId") Long perguntaId);
 
 }
