999942e9aed0883bb1bbeda15db118979dc7aca6
SmythyCosta <smythy.costa@hotmail.com>
2020-01-11 01:24:43 -0300

    add Iprova

---
+++ src/main/java/qiquestoes/persistence/repository/IProvaRepository.java
@@ -0,0 +1,8 @@
+package qiquestoes.persistence.repository;
+
+import qiquestoes.persistence.model.Concurso;
+import qiquestoes.persistence.model.Prova;
+
+public interface IProvaRepository extends IBaseCustomRepository<Prova> {
+
+}
