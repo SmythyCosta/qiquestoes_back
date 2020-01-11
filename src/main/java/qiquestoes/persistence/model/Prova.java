2374c9da1a6c48fb6604f9bfe5b59942503303a5
SmythyCosta <smythy.costa@hotmail.com>
2020-01-11 00:52:53 -0300

    add model prova

---
+++ src/main/java/qiquestoes/persistence/model/Prova.java
@@ -0,0 +1,50 @@
+package qiquestoes.persistence.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "tbl_concurso", uniqueConstraints = { 
+		@UniqueConstraint(columnNames = { "nome" }) })
+public class Prova extends BaseModel<Long> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2697091001665057435L;
+
+	@Column(name = "data_aplicacao_prova", nullable = false)
+	private Date dataAplicacaoProva;
+	
+	@OneToMany(mappedBy = "concurso", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonIgnore
+	//@JsonManagedReference
+	private List<Pergunta> pergunta;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JsonBackReference
+	private Concurso concurso;
+
+
+}
