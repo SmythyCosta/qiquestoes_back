90fbd253ef5c9cfccaaf7e6340196966429ae8d0
SmythyCosta <smythy.costa@hotmail.com>
2020-01-10 23:03:01 -0300

    controller concurso

---
+++ src/main/java/qiquestoes/controller/ConcursoController.java
@@ -0,0 +1,17 @@
+package qiquestoes.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import qiquestoes.persistence.model.Assunto;
+import qiquestoes.persistence.model.Concurso;
+
+@RestController
+@RequestMapping(value = "/api/assunto")
+@CrossOrigin(origins = "*")
+@Api()
+public class ConcursoController extends GenericRestController<Concurso> {
+
+}
